73389d1dd52456e7e52d1ba4b43c6e33e987796a
Parveen Chahal <mail.chahal@gmail.com>
2020-09-07 16:02:29 +0000

    Refactored maximum_absolute_difference.py

--- maximum_absolute_difference.py
+++ maximum_absolute_difference.py
@@ -1,19 +1,23 @@
+#
+# https://www.interviewbit.com/problems/maximum-absolute-difference/
+#
+
 def max_diff(A):
     m = -0xfffffffffffffffffffffffffff
-    j = 0
+    j1 = 0
+    j2 = 0
     for i in range(1, len(A)):
         xi = A[i] + i
-        xj = A[j] + j
+        xj = A[j1] + j1
         m = max(m, xi - xj)
         if xi < xj:
-            j = i
-    j = 0
-    for i in range(1, len(A)):
-        xi = i - A[i] 
-        xj = j - A[j]
+            j1 = i
+
+        xi = i - A[i]
+        xj = j2 - A[j2]
         m = max(m, xi - xj)
         if xi < xj:
-            j = i
+            j2 = i
     return m
 
 r = max_diff([-39, -24, 82, 95, 91, -65, 16, -76, -56, 70])

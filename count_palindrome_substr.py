b4c9d59af00e9a90d51f5beb90b878b39d7c3e46
Parveen Chahal <mail.chahal@gmail.com>
2021-06-20 08:17:09 +0530

    Update count_palindrome_substr.py

--- count_palindrome_substr.py
+++ count_palindrome_substr.py
@@ -1,28 +1,21 @@
-def substrCount(n, s):
-    dp = [[]] * n
-    for i in range(n):
-        dp[i] = [False] * n
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        dp = [[False]*n for _ in range(n)]
+        
+        count = 0
+        for i in range(0, n):
+            dp[i][i] = True
+            count += 1
 
-    count = 0
-
-    for i in range(0, n):
-        dp[i][i] = True
-        count += 1
-
-    for size in range(2, n + 1):
-        for i in range(0, n - size + 1):
-            l = i
-            r = i + size - 1
-            if s[l] == s[r]:
-                _l = l + 1
-                _r = r - 1
-                if(_l > _r or dp[_l][_r]):
-                    dp[l][r] = True
-                    count += 1
-    return count
-
-
-if __name__ == '__main__':
-    s = "abcbaba"
-    result = substrCount(len(s), s)
-    print(result)
+        for size in range(2, n + 1):
+            for i in range(0, n - size + 1):
+                l = i
+                r = i + size - 1
+                if s[l] == s[r]:
+                    _l = l + 1
+                    _r = r - 1
+                    if(_l > _r or dp[_l][_r]):
+                        dp[l][r] = True
+                        count += 1
+        return count

2b70ee69badb7dfd8c2767d4293bbe00bce4931b
Parveen Chahal <mail.chahal@gmail.com>
2021-06-20 15:20:23 +0530

    Update unique_paths.py

--- unique_paths.py
+++ unique_paths.py
@@ -1,14 +1,14 @@
-def unique_paths(n, m):
-    mat = [[0 for _ in range(m)] for _ in range(n)]
-    for i in range(m):
-        mat[0][i] = 1
-    for i in range(n):
-        mat[i][0] = 1
+# https://leetcode.com/problems/unique-paths/
 
-    for i in range(1, n):
-        for j in range(1, m):
-            mat[i][j] = mat[i - 1][j] + mat[i][j - 1]
-    return mat[n - 1][m - 1]
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        mat = [[0 for _ in range(m)] for _ in range(n)]
+        for i in range(m):
+            mat[0][i] = 1
+        for i in range(n):
+            mat[i][0] = 1
 
-r = unique_paths(3, 5)
-print(r)
+        for i in range(1, n):
+            for j in range(1, m):
+                mat[i][j] = mat[i - 1][j] + mat[i][j - 1]
+        return mat[n - 1][m - 1]

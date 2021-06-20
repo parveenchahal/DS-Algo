711a0791446899abf847a6bcd6c158c9eacc81ff
Parveen Chahal <mail.chahal@gmail.com>
2021-06-21 00:37:29 +0530

    Update excel_column_number.py

--- excel_column_number.py
+++ excel_column_number.py
@@ -1,6 +1,4 @@
 # https://www.interviewbit.com/problems/excel-column-number/
-# https://www.interviewbit.com/problems/excel-column-title/
-
 class Solution:
     def titleToNumber(self, s):
         ABC = {chr(i): i - ord('A') + 1 for i in range(ord('A'), ord('A') + 26)}
@@ -11,18 +9,18 @@ class Solution:
             result += ABC[s[i]]
         return result
 
-def excel_column_string(n):
-    ABC = {i: chr(64 + i) for i in range(1, 26)}
-    ABC[0] = 'Z'
-    result = ""
-    while n > 0:
-        r = n % 26
-        n = (n // 26)
-        if r == 0:
-            n -= 1
-        result = ABC[r] + result
-    return result
 
-x = "JKDGDSFGFNNNJ"
-
-assert x == excel_column_string(excel_column_number(x))
+# https://www.interviewbit.com/problems/excel-column-title/
+class Solution:
+    def convertToTitle(self, n):
+        ABC = {i + 1: chr(ord('A') + i) for i in range(0, 26)}
+        result = ""
+        while n > 0:
+            r = n % 26
+            n = (n // 26)
+            if r == 0:
+                result = 'Z' + result
+                n -= 1
+            else:
+                result = ABC[r] + result
+        return result

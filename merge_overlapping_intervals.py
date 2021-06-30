dea22121f84a2cea61b42b1083600fdd244618b8
Parveen Chahal <mail.chahal@gmail.com>
2021-07-01 01:07:02 +0530

    Update merge_overlapping_intervals.py

--- merge_overlapping_intervals.py
+++ merge_overlapping_intervals.py
@@ -1,30 +1,19 @@
-class Interval:
-    def __init__(self, s=0, e=0):
-        self.start = s
-        self.end = e
+# https://leetcode.com/problems/merge-intervals/
 
-def key(interval):
-    return (interval.start, interval.end)
-
-def merge(intervals):
-    n = len(intervals)
-    if n < 2:
-        return intervals
-    intervals = sorted(intervals, key=key)
-    i = 0
-    result = []
-    while i < n:
-        cur_int = Interval(intervals[i].start, intervals[i].end)
-        i += 1
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        n = len(intervals)
+        intervals.sort()
+        result = []
+        cur = intervals[0]
+        i = 1
         while i < n:
-            if intervals[i].start <= cur_int.end:
-                cur_int = Interval(min(cur_int.start, intervals[i].start), max(cur_int.end, intervals[i].end))
+            if intervals[i][0] <= cur[1]:
+                cur[1] = max(cur[1], intervals[i][1])
                 i += 1
-            else:
-                break
-        result.append(cur_int)
-    return result
-
-
-r = merge([Interval(1,3), Interval(2,6), Interval(8,10), Interval(15,18)])
-print(r)
+                continue
+            result.append(cur)
+            cur = intervals[i]
+            i += 1
+        result.append(cur)
+        return result

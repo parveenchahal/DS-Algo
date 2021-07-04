5f3d04b5b12869b2e8474122f893b9f59494e24d
Parveen Chahal <mail.chahal@gmail.com>
2021-07-04 18:46:08 +0530

    Update two_sum.py

--- two_sum.py
+++ two_sum.py
@@ -12,4 +12,20 @@ class Solution:
                 return sorted([m[x], i])
             m[nums[i]] = i
         return []
-                
+
+
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        n = len(numbers)
+        i = 0
+        j = n - 1
+        while i < j:
+            s = numbers[i] + numbers[j]
+            if s < target:
+                i += 1
+            elif s > target:
+                j -= 1
+            else:
+                return [i + 1, j + 1]

8983b08b0e2e679916107af011344ae131f980d3
Parveen Chahal <mail.chahal@gmail.com>
2021-07-08 08:50:50 +0530

    Create integer_to_roman.py

---
+++ integer_to_roman.py
@@ -0,0 +1,47 @@
+# https://leetcode.com/problems/integer-to-roman/
+
+class Solution:
+    def _get_values(self, x):
+        if x == 0:
+            return ''
+        
+        m = [
+            {1: 'I', 5: 'V'},
+            {1: 'X', 5: 'L'},
+            {1: 'C', 5: 'D'},
+            {1: 'M'}
+        ]
+        
+        n_zeros = 0
+        t = x
+        while True:
+            t //= 10
+            if t == 0:
+                break
+            n_zeros += 1
+        
+        f = int(str(x)[0])
+        if f == 1:
+            return m[n_zeros][1]
+        if f > 1 and f <= 3:
+            return m[n_zeros][1] * f
+        if f == 4:
+            return m[n_zeros][1] + m[n_zeros][5]
+        if f == 5:
+            return m[n_zeros][5]
+        if f > 5 and f < 9:
+            return m[n_zeros][5] + (m[n_zeros][1] * (f - 5))
+        if f == 9:
+            return m[n_zeros][1] + m[n_zeros + 1][1]
+    
+    def intToRoman(self, num: int) -> str:
+        result = ''
+        i = 1
+        while True:
+            x = int(num % (10 ** i))
+            result = self._get_values(x) + result
+            num -= x
+            if num <= 0:
+                break
+            i += 1
+        return result

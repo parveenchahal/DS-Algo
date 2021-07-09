5a5eac12aef7c60caab6873a2d0cb5802c0cf8ae
Parveen Chahal <mail.chahal@gmail.com>
2021-07-10 02:54:03 +0530

    Update maximum_sum_contiguous_subarray.py

--- maximum_sum_contiguous_subarray.py
+++ maximum_sum_contiguous_subarray.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/maximum-subarray/
 
+# Method 1 (With extra memory)
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
@@ -12,4 +13,18 @@ class Solution:
         for i in range(1, n + 1):
             res = max(res, dp[i] - min_so_far)
             min_so_far = min(min_so_far, dp[i])
+        return res
+
+
+# Method 2 (Without extra memory)
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        n = len(nums)
+        res = -0x800000000000000
+        min_so_far = 0
+        s = 0
+        for i in range(n):
+            s += nums[i]
+            res = max(res, s - min_so_far)
+            min_so_far = min(min_so_far, s)
         return res

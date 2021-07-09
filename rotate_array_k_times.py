253a6c58a4e5d16cd0da0b729987d99aff05bad1
Parveen Chahal <mail.chahal@gmail.com>
2021-07-10 03:05:59 +0530

    Create rotate_array_k_times.py

---
+++ rotate_array_k_times.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/rotate-array/
+
+class Solution:
+    def _reverse(self, nums, i, j):
+        while i < j:
+            nums[i], nums[j] = nums[j], nums[i]
+            i += 1
+            j -= 1
+        
+    def rotate(self, nums: List[int], k: int) -> None:
+        if k <= 0:
+            return
+        
+        n = len(nums)
+        k = k % n
+        
+        self._reverse(nums, 0, n - 1)
+        self._reverse(nums, 0, k - 1)
+        self._reverse(nums, k, n - 1)

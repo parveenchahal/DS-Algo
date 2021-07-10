014b42ff091b879ff1324277eed9b5ec9f9f88ce
Parveen Chahal <mail.chahal@gmail.com>
2021-07-10 06:27:20 +0530

    Update word_break_problem.py

--- word_break_problem.py
+++ word_break_problem.py
@@ -2,6 +2,22 @@
 
 # Method 1 (DP)
 class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        d = set(wordDict)
+        dp = [False] * (n + 1)
+        dp[0] = True
+        
+        for size in range(1, n + 1):
+            for i in range(size):
+                if dp[i] and s[i:size] in d:
+                    dp[size] = True
+                    break
+        return dp[n]
+
+
+# Method 2 (DP)
+class Solution:
     def wordBreak(self, st: str, wordDict: List[str]) -> bool:
         n = len(st)
         d = set(wordDict)

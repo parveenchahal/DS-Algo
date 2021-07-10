901a7e6ed98a9d3ad450ac0ff4a1b5959f9e5577
Parveen Chahal <mail.chahal@gmail.com>
2021-07-10 09:25:10 +0530

    Update integer_to_roman.py

--- integer_to_roman.py
+++ integer_to_roman.py
@@ -1,5 +1,16 @@
 # https://leetcode.com/problems/integer-to-roman/
 
+# Method 1
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        thousands = ["", "M", "MM", "MMM"]
+        hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        return (thousands[num // 1000] + hundreds[num % 1000 // 100] + tens[num % 100 // 10] + ones[num % 10])
+
+
+# Method 2
 class Solution:
     def _get_values(self, x):
         if x == 0:

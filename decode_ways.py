d63e0e0c1935884125ac29634347d2e2966f5cba
Parveen Chahal <mail.chahal@gmail.com>
2021-07-16 05:10:39 +0530

    Update decode_ways.py

--- decode_ways.py
+++ decode_ways.py
@@ -1,5 +1,34 @@
 # https://leetcode.com/problems/decode-ways/
 
+# Method 1 (Constant space)
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        n = len(s)
+        d1 = 0
+        d2 = 0
+        
+        if s[0] != '0':
+            d2 = 1
+        
+        if n == 1:
+            return d2
+        
+        if s[1] != '0':
+            d1 = d2
+        
+        x = int(s[:2])
+        if x >= 10 and x <= 26:
+            d1 += 1
+        
+        for i in range(2, n):
+            d = 0
+            if s[i] !='0':
+                d = d1
+            x = int(s[i - 1: i + 1])
+            if x >= 10 and x <= 26:
+                d += d2
+            d1, d2 = d, d1
+        return d1
 
 
 # Method 2 (DP: Extra space)

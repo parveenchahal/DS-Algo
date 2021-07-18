2c9b2ac219558ab81500df96775bc600c4ab09bf
Parveen Chahal <mail.chahal@gmail.com>
2021-07-18 05:32:26 +0530

    Update reverse_nodes_in_k_group.py

--- reverse_nodes_in_k_group.py
+++ reverse_nodes_in_k_group.py
@@ -1,5 +1,6 @@
 # https://leetcode.com/problems/reverse-nodes-in-k-group/
 
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):
@@ -7,43 +8,40 @@
 #         self.next = next
 class Solution:
     
-    def _reverse(self, ptr, k):
-        end = ptr
-        pre = None
-        while ptr is not None and k:
-            nex = ptr.next
-            ptr.next = pre
-            pre = ptr
-            ptr = nex
-            k -= 1
-        end.next = ptr
+    def _reverse(self, head, end):
+        pre = end
+        while head != end:
+            next = head.next
+            head.next = pre
+            pre = head
+            head = next
         return pre
     
-    def _count(self, head):
-        c = 0
-        while head is not None:
-            head = head.next
-            c += 1
-        return c
+    def _find_end_node(self, ptr, k):
+        end = ptr
+        i = 0
+        while i < k:
+            if end is None:
+                return None, False
+            end = end.next
+            i += 1
+        return end, True
     
     def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
         if k <= 1:
             return head
-        n = self._count(head)
-        last_op = n - (n % k)
-        new_node = None
-        ptr = head
-        pre = None
-        i = 0
-        while ptr is not None and i < last_op:
-            if i % k == 0:
-                x = self._reverse(ptr, k)
-                if new_node is None:
-                    new_node = x
-                else:
-                    pre.next = x
-                ptr = x
+        end, found = self._find_end_node(head, k)
+        if not found:
+            return head
+        
+        pre = head
+        head = self._reverse(head, end)
+        ptr = end
+        while ptr != None:
+            end, found = self._find_end_node(ptr, k)
+            if not found:
+                return head
+            pre.next = self._reverse(ptr, end)
             pre = ptr
-            ptr = ptr.next
-            i += 1
-        return new_node
+            ptr = end
+        return head

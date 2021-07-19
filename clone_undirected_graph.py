e2e88b2e14d5d3186507a6917ec3ee90066b7c03
Parveen Chahal <mail.chahal@gmail.com>
2021-07-19 20:51:12 +0530

    Update clone_undirected_graph.py

--- clone_undirected_graph.py
+++ clone_undirected_graph.py
@@ -1,50 +1,35 @@
-# https://www.interviewbit.com/problems/clone-graph/
+# https://leetcode.com/problems/clone-graph/
 
-# Definition for a undirected graph node
-class UndirectedGraphNode:
-    def __init__(self, x):
-        self.label = x
-        self.neighbors = []
-
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
 class Solution:
-    # @param node, a undirected graph node
-    # @return a undirected graph node
-    def cloneGraph(self, node):
-        return self._cloneGraph(node, None, set(), {})
-
-
-    def _cloneGraph(self, node, new_node, visited, new_node_map):
+    
+    def _clone_graph(self, node, visited, cloned_map):
+        if node.val in visited:
+            return
+        visited.add(node.val)
+        
+        for x in node.neighbors:
+            if x.val not in cloned_map:
+                cloned_map[x.val] = Node(x.val, [])
+            
+            # Added Edge for cloned nodes
+            cloned_map[node.val].neighbors.append(cloned_map[x.val])
+            self._clone_graph(x, visited, cloned_map)
+    
+    def cloneGraph(self, node: 'Node') -> 'Node':
         if node is None:
             return
-        visited.add(node)
-        if new_node is None:
-            new_node = UndirectedGraphNode(node.label)
-            new_node_map[new_node.label] = new_node
-
-        for n in node.neighbors:
-            if n not in visited:
-                n_t = UndirectedGraphNode(n.label)
-                new_node_map[n_t.label] = n_t
-                self._cloneGraph(n, n_t, visited, new_node_map)
-            new_node_map[node.label].neighbors.append(new_node_map[n.label])
-        return new_node
-
-n703 = UndirectedGraphNode(703)
-n43 = UndirectedGraphNode(43)
-n279 = UndirectedGraphNode(279)
-
-n703.neighbors.append(n279)
-n703.neighbors.append(n703)
-n703.neighbors.append(n43)
-
-n279.neighbors.append(n703)
-n279.neighbors.append(n43)
-
-
-n43.neighbors.append(n279)
-n43.neighbors.append(n43)
-n43.neighbors.append(n703)
-
-
-new_node = Solution().cloneGraph(n703)
-print(new_node)
+        
+        visited = set()
+        cloned_map = {}
+        
+        cloned_map[node.val] = Node(node.val, [])
+        self._clone_graph(node, visited, cloned_map)
+        
+        return cloned_map[node.val]

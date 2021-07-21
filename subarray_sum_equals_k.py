42b910facdb2c0347d41508fc6a144dd35cda730
Parveen Chahal <mail.chahal@gmail.com>
2021-07-21 17:29:58 +0530

    Update subarray_sum_equals_k.py

--- subarray_sum_equals_k.py
+++ subarray_sum_equals_k.py
@@ -3,21 +3,19 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         n = len(nums)
-        sum_so_far = [0] * (n + 1)
-        for i in range(1, n + 1):
-            sum_so_far[i] = sum_so_far[i - 1] + nums[i - 1]
-        
+        sum_so_far = 0
         count = 0
         m = {0: 1}
-        for i in range(1, n + 1):
-            x = sum_so_far[i] - k
+        for i in range(n):
+            sum_so_far += nums[i]
+            x = sum_so_far - k
             try:
                 count += m[x]
             except KeyError:
                 pass
             
             try:
-                m[sum_so_far[i]] += 1
+                m[sum_so_far] += 1
             except KeyError:
-                m[sum_so_far[i]] = 1
+                m[sum_so_far] = 1
         return count

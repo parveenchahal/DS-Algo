2996dd8fba870c8723ce93bcfb2285545038f489
Parveen Chahal <mail.chahal@gmail.com>
2021-08-21 23:46:58 +0530

    Create longest_valid_parentheses.py

---
+++ longest_valid_parentheses.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/longest-valid-parentheses/
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        n = len(s)
+        st = [-1]
+        res = 0
+        for i in range(n):
+            c = s[i]
+            if c == '(':
+                st.append(i)
+            else:
+                if len(st) > 1 and s[st[-1]] == '(':
+                    st.pop()
+                    res = max(res, i - st[-1])
+                else:
+                    st = [i]
+        res = max(res, n - 1 - st[-1])
+        return res

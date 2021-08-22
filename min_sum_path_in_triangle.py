6ba45d5ffa925dc569072fe5f62268b798331815
Parveen Chahal <mail.chahal@gmail.com>
2021-08-22 18:12:36 +0530

    Update min_sum_path_in_triangle.py

--- min_sum_path_in_triangle.py
+++ min_sum_path_in_triangle.py
@@ -10,10 +10,10 @@ def minimum_total(A):
         
 if __name__ == '__main__':
     x = minimum_total([
-        [2],
-        [3,4],
-    [6,5,7],
-    [4,1,8,3]
+            [2],
+           [3,4],
+          [6,5,7],
+         [4,1,8,3]
     ])
     
     print(x)

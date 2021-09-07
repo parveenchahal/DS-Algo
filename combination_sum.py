e373b16c7b6476b47b4cc71e02b23e418bd05d7d
Parveen Chahal <mail.chahal@gmail.com>
2021-09-08 01:17:39 +0530

    Update combination_sum.py

--- combination_sum.py
+++ combination_sum.py
@@ -1,5 +1,25 @@
 # https://leetcode.com/problems/combination-sum/
 
+# Method 1 (Backtracking)
+class Solution:
+    def _combination(self, i, buf, target, candidates, n, res):
+        if target < 0:
+            return
+        if target == 0:
+            res.append(list(buf))
+        for j in  range(i, n):
+            buf.append(candidates[j])
+            self._combination(j, buf, target - candidates[j], candidates, n, res)
+            buf.pop()
+    
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        n = len(candidates)
+        res = []
+        self._combination(0, [], target, candidates, n, res)
+        return res
+
+
+# Method 2 (DP)
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         n = len(candidates)

22328969d5120db14afff62acd3460cf7d7c863d
Parveen Chahal <mail.chahal@gmail.com>
2021-09-09 07:34:34 +0530

    Update minimum_remove_to_make_valid_parentheses.py

--- minimum_remove_to_make_valid_parentheses.py
+++ minimum_remove_to_make_valid_parentheses.py
@@ -1,6 +1,40 @@
 # https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
 
-# Method 1 Using stack
+# Method 1 using two pass
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        n = len(s)
+        idx_remove = set()
+        bal = 0
+        for i in range(n):
+            c = s[i]
+            if c == '(':
+                bal += 1
+            elif c == ')':
+                bal -= 1
+            if bal < 0:
+                idx_remove.add(i)
+                bal = 0
+        
+        bal = 0
+        for i in range(n - 1, -1, -1):
+            c = s[i]
+            if c == ')':
+                bal += 1
+            elif c == '(':
+                bal -= 1
+            if bal < 0:
+                idx_remove.add(i)
+                bal = 0
+        
+        res_str = []
+        for i,c in enumerate(s):
+            if i not in idx_remove:
+                res_str.append(c)
+        return ''.join(res_str)
+
+
+# Method 2 Using stack
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
         idx_remove = set()

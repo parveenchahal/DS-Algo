ba41a8f06e5afb6f668a16423f87f9183522b156
Parveen Chahal <mail.chahal@gmail.com>
2021-09-11 22:21:59 +0530

    Update trapping_rain_water.py

--- trapping_rain_water.py
+++ trapping_rain_water.py
@@ -1,5 +1,30 @@
 # https://leetcode.com/problems/trapping-rain-water/
 
+
+# Method 1 using two pointer and without extra memory
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        n = len(height)
+        max_left = 0
+        max_right = 0
+        left = 0
+        right= n - 1
+        total = 0
+        
+        while left < right:
+            max_left = max(max_left, height[left])
+            max_right = max(max_right, height[right])
+            if height[left] <= height[right]:
+                total += min(max_left, max_right) - height[left]
+                left += 1
+            else:
+                total += min(max_left, max_right) - height[right]
+                right -= 1
+        
+        return total
+        
+
+# Method 2 using extra memory
 class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)

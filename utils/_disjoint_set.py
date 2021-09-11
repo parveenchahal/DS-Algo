693862f319c0babdba3139fd5473e14c08ec3e83
Parveen Chahal <mail.chahal@gmail.com>
2021-09-11 10:35:13 +0530

    Update _disjoint_set.py

--- utils/_disjoint_set.py
+++ utils/_disjoint_set.py
@@ -1,37 +1,50 @@
-from typing import Union, Any, List
-
 class DisjointSet():
 
     _map: dict
+    _rank: dict
 
     def __init__(self):
         self._map = {}
+        self._rank = {}
+        
+    def make_set(self, x):
+        self._map[x] = x
+        self._rank[x] = 0
 
-    def add_set(self, v: Union[Any, List[Any]]):
-        if isinstance(v, list):
-            for x in v:
-                self._map[x] = v[0]
+    def union(self, x, y):
+        a = self.find(x)
+        b = self.find(y)
+        if a == b:
+            return
+        if self._rank[a] == self._rank[b]:
+            self._map[b] = a
+            self._rank[a] += 1
+            del self._rank[b]
+        elif self._rank[a] > self._rank[b]:
+            self._map[b] = a
+            del self._rank[b]
         else:
-            self._map[v] = v
+            self._map[a] = b
+            del self._rank[a]
 
-    def union(self, a, b):
-        _a = self.find(a)
-        _b = self.find(b)
-        self._map[_b] = _a
-
-    def find(self, v):
+    def find(self, x):
         try:
-            self._map[v]
+            self._map[x]
         except KeyError:
             return None
-        t = v
-        while(self._map[t] != t):
-            t = self._map[t]
-
+        p = x
+        while self._map[p] != p:
+            p = self._map[p]
         # Compression
-        self._map[v] = t
-        
-        return t
+        self._map[x] = p
+        return p
+    
+    def distinct_set_count(self):
+        count = 0
+        for k,v in self._map.items():
+            if k == v:
+                count += 1
+        return count
 
     def __str__(self):
         return str(self._map)

4a879069fad515be714b6253cd52186de3176c59
Parveen Chahal <mail.chahal@gmail.com>
2021-09-12 20:24:39 +0530

    Update make_sum_divisible_by_p.py

--- make_sum_divisible_by_p.py
+++ make_sum_divisible_by_p.py
@@ -6,6 +6,30 @@ from typing import List
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
         n = len(nums)
+        s = sum(nums)
+        r = s % p
+        if r == 0:
+            return 0
+        sum_so_far = 0
+        m = {0: -1}
+        MAX = n
+        result = MAX
+        for i in range(n):
+            sum_so_far += nums[i]
+            sum_so_far = sum_so_far % p
+            x = sum_so_far - r
+            if x < 0:
+                x += p
+            if x in m:
+                result = min(result, i - m[x])
+            m[sum_so_far] = i
+        return result if result != MAX else -1
+
+
+# Method 2
+class Solution:
+    def minSubarray(self, nums: List[int], p: int) -> int:
+        n = len(nums)
         r = sum(nums) % p
         if r == 0:
             return 0
@@ -21,31 +45,3 @@ class Solution:
             if t in m:
                 res = min(res, i - m[t])
         return res if res < n else -1
-
-
-# Method 2
-def min_subarray(nums: List[int], p: int) -> int:
-    n = len(nums)
-    s = sum(nums)
-    r = s % p
-    if r == 0:
-        return 0
-    sum_so_far = 0
-    m = {0: -1}
-    MAX = n
-    result = MAX
-    for i in range(n):
-        sum_so_far += nums[i]
-        x = sum_so_far % p - r
-        if x < 0:
-            x += p
-        if x in m:
-            result = min(result, i - m[x])
-        m[sum_so_far % p] = i
-    return result if result != MAX else -1
-
-r = min_subarray([3,1,4,2], 6)
-assert r == 1
-
-r = min_subarray([6,3,5,2], 9)
-assert r == 2

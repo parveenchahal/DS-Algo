93400b84331e58bda9b11ffcdd63727a46e4736c
Parveen Chahal <mail.chahal@gmail.com>
2021-09-14 17:09:35 +0530

    Update critical_connections_in_a_network.py

--- critical_connections_in_a_network.py
+++ critical_connections_in_a_network.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/critical-connections-in-a-network/
 
 class Solution:
-    def _dfs(self, g, u, par, visited, cur_rank, dis_rank, low_rank, ap):
+    def _dfs(self, g, u, par, visited, cur_rank, dis_rank, low_rank, res):
         if u in visited:
             return low_rank[u]
         visited.add(u)
@@ -13,14 +13,12 @@ class Solution:
                 continue
             if v not in visited:
                 children += 1
-            l = self._dfs(g, v, u, visited, cur_rank, dis_rank, low_rank, ap)
+            v_low = self._dfs(g, v, u, visited, cur_rank, dis_rank, low_rank, res)
             
             # This part if different then usual "Find Articulation Algo"
-            if l > dis_rank[u]:
-                t = [u, v]
-                t.sort()
-                ap.add(tuple(t))
-            low_rank[u] = min(low_rank[u], l, dis_rank[v])
+            if v_low > dis_rank[u]:
+                res.add((u, v))
+            low_rank[u] = min(low_rank[u], v_low, dis_rank[v])
         return low_rank[u]
     
     def criticalConnections(self, n: int, connections: List[List[int]]) -> List[List[int]]:
@@ -29,8 +27,8 @@ class Solution:
             u, v = con
             g[u].add(v)
             g[v].add(u)
-        ap = set()
+        res = set()
         dis_rank = {}
         low_rank = {}
-        self._dfs(g, 0, None, set(), 0, dis_rank, low_rank, ap)
-        return ap
+        self._dfs(g, 0, None, set(), 0, dis_rank, low_rank, res)
+        return res

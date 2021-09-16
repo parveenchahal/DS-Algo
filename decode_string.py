39396f29d860629285b3495ee448547819a10aad
Parveen Chahal <mail.chahal@gmail.com>
2021-09-17 01:32:17 +0530

    Update decode_string.py

--- decode_string.py
+++ decode_string.py
@@ -1,25 +1,40 @@
 # https://leetcode.com/problems/decode-string/
 
 
+BRAC = 0
+INT = 1
+ABC = 2
+
 class Solution:
+    def add(st, typ, val_arr):
+        if len(st) <= 0 or typ != st[-1][0]:
+            st.append([typ, []])
+        st[-1][1].extend(val_arr)
+        
+    def remove(st):
+        abc = []
+        rf = ['1']
+        if len(st) > 0 and st[-1][0] == ABC:
+            _, abc = st.pop()
+        if len(st) > 0 and st[-1][0] == BRAC:
+            st.pop()
+        if len(st) > 0 and st[-1][0] == INT:
+            _, rf = st.pop()
+        rf = int(''.join(rf))
+        return abc * rf
+    
     def decodeString(self, s: str) -> str:
         n = len(s)
         st = []
         for i in range(n):
-            if s[i] != ']':
-                if s[i] >= '0' and s[i] <= '9' and len(st) > 0 and st[-1] >= '0' and st[-1] <= '9':
-                    st[-1] += s[i]
-                else:
-                    st.append(s[i])
+            c = s[i]
+            if c >= '0' and c <= '9':
+                Solution.add(st, INT, [c])
+            elif c >= 'a' and c <= 'z':
+                Solution.add(st, ABC, [c])
+            elif c == '[':
+                Solution.add(st, BRAC, [c])
             else:
-                rs = ''
-                while len(st) > 0 and st[-1] != '[':
-                    rs = st.pop() + rs
-                st.pop()
-                rf = ''
-                while len(st) > 0 and st[-1] >= '0' and st[-1] <= '9':
-                    rf = st.pop() + rf
-                t = rs * int(rf)
-                for c in t:
-                    st.append(c)
-        return ''.join(st)
+                t = Solution.remove(st)
+                Solution.add(st, ABC, t)
+        return ''.join(st[0][1])

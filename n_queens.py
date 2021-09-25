21b30bde808e9cca1c3ae84ba72334660378e266
Parveen Chahal <mail.chahal@gmail.com>
2021-09-25 16:45:17 +0530

    Update n_queens.py

--- n_queens.py
+++ n_queens.py
@@ -1,5 +1,50 @@
 # https://leetcode.com/problems/n-queens/
 
+# Method 1
+
+class Solution:
+    
+    def _is_valid(self, i, j, n, rows_set, cols_set, diag_set, anti_diag_set):
+        return i not in rows_set \
+            and j not in cols_set \
+            and (i + j) not in diag_set \
+            and (n - 1 - (j - i)) not in anti_diag_set
+    
+    def _solve_n_queen(self, board, n, i, rows_set, cols_set, diag_set, anti_diag_set, res):
+        if i >= n:
+            t = []
+            for x in board:
+                t.append(''.join(x))
+            res.append(t)
+            return
+        for j in range(n):
+            if self._is_valid(i, j, n, rows_set, cols_set, diag_set, anti_diag_set):
+                board[i][j] = 'Q'
+                rows_set.add(i)
+                cols_set.add(j)
+                d = i + j
+                diag_set.add(d)
+                ad = n - 1 - (j - i)
+                anti_diag_set.add(ad)
+                self._solve_n_queen(board, n, i + 1, rows_set, cols_set, diag_set, anti_diag_set, res)
+                rows_set.discard(i)
+                cols_set.discard(j)
+                diag_set.discard(d)
+                anti_diag_set.discard(ad)
+                board[i][j] = '.'
+    
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+        board = [['.'] * n for _ in range(n)]
+        rows_set = set()
+        cols_set = set()
+        diag_set = set()
+        anti_diag_set = set()
+        self._solve_n_queen(board, n, 0, rows_set, cols_set, diag_set, anti_diag_set, res)
+        return res
+
+
+# Method 2
 
 class Solution:
     

bd65d65523abfed68eb572babc347c9b8a247d19
Parveen Chahal <mail.chahal@gmail.com>
2021-09-25 19:06:30 +0530

    Update largest_rectangle_in_histogram.py

--- largest_rectangle_in_histogram.py
+++ largest_rectangle_in_histogram.py
@@ -2,6 +2,39 @@
 
 from typing import List
 
+# Method 1
+class Solution:
+
+    def _find_just_smaller_indices(self, heights):
+        n = len(heights)
+        left_res = []
+        st = [-1]
+        for i,h in enumerate(heights):
+            while len(st) > 1 and heights[st[-1]] >= h:
+                st.pop()
+            left_res.append(st[-1])
+            st.append(i)
+        
+        right_res = []
+        st = [n]
+        for i,h in reversed(list(enumerate(heights))):
+            while len(st) > 1 and heights[st[-1]] >= h:
+                st.pop()
+            right_res.append(st[-1])
+            st.append(i)
+        right_res.reverse()
+        return left_res, right_res
+
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        left_just_smaller, right_just_smaller = self._find_just_smaller_indices(heights)
+        res = 0
+        for i,h in enumerate(heights):
+            area = h * (right_just_smaller[i] - left_just_smaller[i] - 1)
+            res = max(res, area)
+        return res
+
+
+# Method 2
 class Solution:
 
     def _pop_until_and_get_max_area(self, h, st, i):
@@ -27,7 +60,4 @@ class Solution:
             
         if len(st) > 0:
             result = max(result, self._pop_until_and_get_max_area(h, st, n))
-        print(result)
         return result
-
-assert 10 == Solution().largestRectangleArea([2,1,5,6,2,3])

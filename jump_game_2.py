656b9f262efa7bb7802b9a4ee33da2004d585cab
Parveen Chahal <mail.chahal@gmail.com>
2021-10-11 22:35:59 +0530

    Update jump_game_2.py

--- jump_game_2.py
+++ jump_game_2.py
@@ -1,8 +1,22 @@
 # https://leetcode.com/problems/jump-game-ii/
 # https://www.interviewbit.com/problems/min-jumps-array/
 
-from typing import List
+# Method 1 (Without using extra space)
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        n = len(nums)
+        res = 0
+        max_reach = 0
+        cur_jump = 0
+        for i in range(n - 1):
+            max_reach = max(max_reach, nums[i] + i)
+            if i == cur_jump:
+                res += 1
+                cur_jump = max_reach
+        return res
 
+
+# Method 2 (Using extra space)
 class Solution:
     def jump(self, nums: List[int]) -> int:
         n = len(nums)

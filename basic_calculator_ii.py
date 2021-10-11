a677a76fcaaa488bd2fd0174811afc1365d710b2
Parveen Chahal <mail.chahal@gmail.com>
2021-10-11 19:11:00 +0530

    Update basic_calculator_ii.py

--- basic_calculator_ii.py
+++ basic_calculator_ii.py
@@ -1,7 +1,38 @@
 # https://leetcode.com/problems/basic-calculator-ii/
 
 
-# Method 1 (Using Stack)
+# Method 1 (Without stack)
+class Solution:
+    def calculate(self, s: str) -> int:
+        s += '+'
+        res = 0
+        last_num = 0
+        cur_num = 0
+        last_op = '+'
+        for c in s:
+            if c == ' ':
+                continue
+            if c.isdigit():
+                cur_num = cur_num * 10 + int(c)
+            else:
+                if last_op == '+':
+                    res += last_num
+                    last_num = cur_num
+                elif last_op == '-':
+                    res += last_num
+                    last_num = -cur_num
+                elif last_op == '*':
+                    last_num *= cur_num
+                elif last_op == '/':
+                    last_num = int(last_num / cur_num)
+                else:
+                    raise
+                cur_num = 0
+                last_op = c
+        return res + last_num
+
+
+# Method 2 (Using Stack)
 class Solution:
     def calculate(self, s: str) -> int:
         s += '+'

ba70a60caec5ff890c0a9ba8745e7d5d1cb65caa
Parveen Chahal <mail.chahal@gmail.com>
2021-10-30 23:45:53 +0530

    Update employee_free_time.py

--- employee_free_time.py
+++ employee_free_time.py
@@ -10,32 +10,67 @@ class Interval:
 """
 
 class Solution:
-    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
-        ins = []
-        outs = []
+    
+    def _merge_two_sorted_lists(self, l1, l2):
+        n1 = len(l1)
+        n2 = len(l2)
+        i = 0
+        j = 0
+        res = []
+        while i < n1 and j < n2:
+            if l1[i] <= l2[j]:
+                res.append(l1[i])
+                i += 1
+            else:
+                res.append(l2[j])
+                j += 1
+        while i < n1:
+            res.append(l1[i])
+            i += 1
+        while j < n2:
+            res.append(l2[j])
+            j += 1
+        return res
+    
+    
+    def _merge_k_sorted_lists(self, q):
+        if len(q) == 0:
+            raise ValueError('Empty Queue')
+        
+        while len(q) > 1:
+            a = q.popleft()
+            b = q.popleft()
+            c = self._merge_two_sorted_lists(a, b)
+            q.append(c)
+        
+        return q[0]
+    
+    
+    def _separate_ins_outs_in_sorted(self, schedule):
+        ins_q = deque()
+        outs_q = deque()
         for sc in schedule:
-            for x in sc:
-                ins.append(x.start)
-                outs.append(x.end)
-        ins.sort()
-        outs.sort()
+            ins_q.append([x.start for x in sc])
+            outs_q.append([x.end for x in sc])
+        return self._merge_k_sorted_lists(ins_q), self._merge_k_sorted_lists(outs_q)
+    
+    
+    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
+        ins, outs = self._separate_ins_outs_in_sorted(schedule)
         n = len(ins)
         i = j = 0
         cur_time = 0
-        pre_time = 0
         count = 0
         res = []
         while i < n and j < n:
             if ins[i] <= outs[j]:
-                pre_time, cur_time = cur_time, ins[i]
-                i += 1
                 count += 1
+                if len(res) > 0 and len(res[-1]) == 1:
+                    res[-1].append(ins[i])
+                i += 1
             else:
-                pre_time, cur_time = cur_time, outs[j]
-                j += 1
                 count -= 1
-            if count == 0:
-                res.append([cur_time])
-            elif len(res) > 0 and len(res[-1]) == 1:
-                res[-1].append(cur_time)
+                if count == 0:
+                    res.append([outs[j]])
+                j += 1
         return list(map(lambda x: Interval(x[0], x[1]), res))

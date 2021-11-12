e7ab06c70b8df2a7f8740772aae9ef9a9414b2c3
Parveen Chahal <mail.chahal@gmail.com>
2021-11-13 02:33:25 +0530

    Update minimum_degree_of_a_connected_trio_in_a_graph.py

--- minimum_degree_of_a_connected_trio_in_a_graph.py
+++ minimum_degree_of_a_connected_trio_in_a_graph.py
@@ -1,6 +1,33 @@
 # https://leetcode.com/problems/minimum-degree-of-a-connected-trio-in-a-graph/
 
 
+# Method 1
+class Solution:
+    MAX = 0x7ffffffffffff
+    
+    def minTrioDegree(self, n: int, edges: List[List[int]]) -> int:
+        g = defaultdict(set)
+        for edge in edges:
+            u, v = edge
+            g[u].add(v)
+            g[v].add(u)
+        degree = {}
+        for x in g:
+            degree[x] = len(g[x])
+        res = self.MAX
+        for u in range(1, n + 1):
+            for v in g[u]:
+                intersections = g[u].intersection(g[v])
+                for common_conn in intersections:
+                    trio_degree = degree[u] + degree[v] + degree[common_conn] - 6
+                    if trio_degree >= 0:
+                        res = min(res, trio_degree)
+                    g[common_conn].discard(u)
+                    g[common_conn].discard(v)
+        return res if res != self.MAX else -1
+
+
+# Method 2
 class Solution:
     MAX = 0x7ffffffffffff
     

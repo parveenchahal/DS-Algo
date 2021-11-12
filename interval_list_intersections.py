2179bfd354cef1c1669b06cb890e863a54ae1425
Parveen Chahal <mail.chahal@gmail.com>
2021-11-12 15:52:04 +0530

    Update interval_list_intersections.py

--- interval_list_intersections.py
+++ interval_list_intersections.py
@@ -3,21 +3,23 @@
 
 class Solution:
     
-    def _intersection(x, y):
-        return [max(x[0], y[0]), min(x[1], y[1])]
+    def _intersection(self, i1, i2):
+        res = max(i1[0], i2[0]), min(i1[1], i2[1])
+        return res if res[0] <= res[1] else None
     
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
         n1 = len(firstList)
         n2 = len(secondList)
-        i = j = 0
+        i = 0
+        j = 0
         res = []
         while i < n1 and j < n2:
-            x = firstList[i]
-            y = secondList[j]
-            t = Solution._intersection(x, y)
-            if t[0] <= t[1]:
-                res.append(t)
-            if x[1] <= y[1]:
+            interval1 = firstList[i]
+            interval2 = secondList[j]
+            intersection = self._intersection(interval1, interval2)
+            if intersection is not None:
+                res.append(intersection)
+            if interval1[1] <= interval2[1]:
                 i += 1
             else:
                 j += 1

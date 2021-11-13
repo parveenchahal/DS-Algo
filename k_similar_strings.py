65f0781d004fcb19f0df0fae6894d396e222180f
Parveen Chahal <mail.chahal@gmail.com>
2021-11-13 23:30:07 +0530

    Update k_similar_strings.py

--- k_similar_strings.py
+++ k_similar_strings.py
@@ -3,41 +3,36 @@
 
 class Solution:
     
-    def _get_conn(self, s, target):
-        n = len(s)
-        
-        # find first unmatch
-        first_unmatch = 0
-        while s[first_unmatch] == target[first_unmatch]:
-            first_unmatch += 1
-        
+    def _get_conn(self, u, target):
+        n = len(u)
+        first_unmatched = 0
+        for c1,c2 in zip(u, target):
+            if c1 != c2:
+                break
+            first_unmatched += 1
+        if first_unmatched >= n:
+            return []
         res = []
-        for i in range(n):
-            if s[i] != target[i]: 
-                if s[i] != target[i] and s[first_unmatch] == target[i]: 
-                    new_s = list(s)
-                    new_s[first_unmatch], new_s[i] = new_s[i], new_s[first_unmatch]
-                    res.append(''.join(new_s))
+        for i in range(first_unmatched + 1, n):
+            c1 = u[i]
+            c2 = target[i]
+            if c1 != c2 and target[first_unmatched] == u[i]:
+                new_str = u[:first_unmatched] + u[i] + u[first_unmatched + 1: i] + u[first_unmatched] + u[i + 1:]
+                res.append(new_str)
         return res
     
-    
-    def _bfs(self, x, target):
-        if x == target:
-            return 0
-        k = 1
-        q = deque([x])
-        visited = set([x])
-        while len(q) > 0:
-            for _ in range(len(q)):
-                u = q.popleft()
-                for v in self._get_conn(u, target):
-                    if v == target:
-                        return k
-                    if v not in visited:
-                        visited.add(v)
-                        q.append(v)
-            k += 1
-        return k
-    
     def kSimilarity(self, s1: str, s2: str) -> int:
-        return self._bfs(s1, s2)
+        if s1 == s2:
+            return 0
+        
+        q = deque([(s1, 0)])
+        visited = set(s1)
+        while len(q) > 0:
+            u, d = q.popleft()
+            for v in self._get_conn(u, s2):
+                if v == s2:
+                    return d + 1
+                if v not in visited:
+                    visited.add(v)
+                    q.append((v, d + 1))
+        return -1

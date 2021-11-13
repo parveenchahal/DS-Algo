26be2f9cbd31e67d097be52b00803a42fd72ffe4
Parveen Chahal <mail.chahal@gmail.com>
2021-11-14 02:58:32 +0530

    Update longest_increasing_path_in_a_matrix.py

--- longest_increasing_path_in_a_matrix.py
+++ longest_increasing_path_in_a_matrix.py
@@ -5,23 +5,17 @@ class Solution:
     
     DIRS = ((-1, 0), (1, 0), (0, -1), (0, 1))
     
-    def _dfs(self, u, matrix, R, C, visited, memo):
+    def _dfs(self, u, matrix, R, C, memo):
         ui, uj = u
-        if u in visited:
-            return 0
         if memo[ui][uj] is not None:
             return memo[ui][uj]
-        visited.add(u)
         memo[ui][uj] = 1
         for dir in self.DIRS:
             vi, vj = ui + dir[0], uj + dir[1]
             if vi < 0 or vi >= R or vj < 0 or vj >= C or matrix[ui][uj] >= matrix[vi][vj]:
                 continue
-            v = (vi, vj)
-            d = self._dfs(v, matrix, R, C, visited, memo)
-            if memo[ui][uj] is not None:
-                memo[ui][uj] = max(memo[ui][uj], d + 1)
-        visited.discard(u)
+            d = self._dfs((vi, vj), matrix, R, C, memo)
+            memo[ui][uj] = max(memo[ui][uj], d + 1)
         return memo[ui][uj]
     
     def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
@@ -30,8 +24,9 @@ class Solution:
         memo = [[None] * C for _ in range(R)]
         for i in range(R):
             for j in range(C):
-                self._dfs((i, j), matrix, R, C, set(), memo)
+                self._dfs((i, j), matrix, R, C, memo)
         res = 1
         for row in memo:
             res = max(res, max(row))
         return res
+    

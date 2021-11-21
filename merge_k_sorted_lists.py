4448346f039a3b1cd2ca5c2561dca979eda1bb62
Parveen Chahal <mail.chahal@gmail.com>
2021-11-21 23:18:50 +0530

    Update merge_k_sorted_lists.py

--- merge_k_sorted_lists.py
+++ merge_k_sorted_lists.py
@@ -8,19 +8,8 @@
 #         self.next = next
 class Solution:
     def _merge(self, head1, head2):
-        if head1 is None:
-            return head2
-        if head2 is None:
-            return head1
-        head = None
-        if head1.val <= head2.val:
-            head = head1
-            head1 = head1.next
-        else:
-            head = head2
-            head2 = head2.next
-        
-        ptr = head
+        # Added a dummy node
+        ptr = head = ListNode(None)
         while head1 is not None and head2 is not None:
             if head1.val <= head2.val:
                 ptr.next = head1
@@ -29,17 +18,11 @@ class Solution:
                 ptr.next = head2
                 head2 = head2.next
             ptr = ptr.next
-        
-        while head1 is not None:
+        if head1 is not None:
             ptr.next = head1
-            ptr = ptr.next
-            head1 = head1.next
-            
-        while head2 is not None:
+        else:
             ptr.next = head2
-            ptr = ptr.next
-            head2 = head2.next
-        return head
+        return head.next
 
     
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:

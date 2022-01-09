2322a84cb4e86ab3794b00da6ac7fc95bdd17e25
Parveen Chahal <mail.chahal@gmail.com>
2022-01-09 22:36:44 +0530

    Create majority_element.py

---
+++ majority_element.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/majority-element/
+
+
+# Method 1 (Boyer-Moore Majority Voting Algorithm)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        majority = None
+        count = 0
+        for x in nums:
+            if count == 0:
+                majority = x
+            
+            if x == majority:
+                count += 1
+            else:
+                count -= 1
+        return majority
+
+# Method 2 (Sorting)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        nums.sort()
+        return nums[len(nums) // 2]

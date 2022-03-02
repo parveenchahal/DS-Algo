31ed0f8c4256a99c5ba023f2dcc8faf99cda82b0
Parveen Chahal <mail.chahal@gmail.com>
2022-03-02 19:42:38 +0530

    Update employee_free_time.py

--- employee_free_time.py
+++ employee_free_time.py
@@ -1,7 +1,32 @@
 # https://leetcode.com/problems/employee-free-time/
 
+# Method 1 (Number of intervals are small in number, this method run faster)
+"""
+# Definition for an Interval.
+class Interval:
+    def __init__(self, start: int = None, end: int = None):
+        self.start = start
+        self.end = end
+"""
 
-# Method 1
+class Solution:
+    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
+        Interval.__repr__ = lambda self: str((self.start, self.end))
+        intervals = [interval for emp_intervals in schedule for interval in emp_intervals]
+        intervals.sort(key=lambda x: (x.start, x.end))
+        
+        res = []
+        end = intervals[0].end
+        for interval in intervals:
+            a, b = interval.start, interval.end
+            if end < a:
+                res.append(Interval(end, a))
+            end = max(b, end)
+        return res
+
+
+
+# Method 2 (If there are large number of employees and large number of intervals per employee then this method works faster)
 """
 # Definition for an Interval.
 class Interval:
